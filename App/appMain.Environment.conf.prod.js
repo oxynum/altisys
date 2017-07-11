0f50d501973a51c8660357fb67fd3c80c77e4800
Hug0xynum <hugo@oxynum.fr>
2017-07-11 13:19:30 +0200

    update config files to connect to back API

--- App/appMain.Environment.conf.prod.js
+++ App/appMain.Environment.conf.prod.js
@@ -1,10 +1,15 @@
 ﻿'use strict';
 var app = angular.module('appMain.Environment.conf', []);
 
+// app.constant("config", {
+//     "url": "http://polluxvm01:8080/api/version",
+//     "urlV1": "http://polluxvm01:8081/",
+//     "baseUrl": "http://polluxvm01/index.main.html"
+// });
 app.constant("config", {
-    "url": "http://polluxvm01:8080/api/version",
-    "urlV1": "http://polluxvm01:8081/",
-    "baseUrl": "http://polluxvm01/index.main.html"
+    "url": "http://159.180.240.236:8182/api/version",
+    "urlV1": "http://159.180.240.236:8183/",
+    "baseUrl": "http://159.180.240.236:8181/index.main.html"
 });
 /* External access */
 /* app.constant("config", {
