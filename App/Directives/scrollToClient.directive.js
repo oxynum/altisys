4c393311f50c607ed0e2b92d65c72fc2dec96464
Simbadev <maxime.gasnier.1@gmail.com>
2017-07-27 18:59:47 +0200

    correct bug 1 - speedy scroll

--- App/Directives/scrollToClient.directive.js
+++ App/Directives/scrollToClient.directive.js
@@ -8,35 +8,28 @@ app.directive("scrollToClient", ['$window', '$document', function ($window, $doc
         link: function(scope, element, attrs) {
             angular.element($window).bind("scroll", function() {
                 if ($window.pageYOffset > $document[0].getElementById('main').offsetTop - 143) {
-
+                    console.log('FIRST STAGE');
                     if (!scope.boolChangeClass) {
                         scope.boolChangeClass = true;
+                        console.log('SECOND STAGE');
                         setTimeout(function(){
                             $('.billing-block .fix-on-scroll').css('height', '170px');
                             $('.billing-block .boxes-wrap .box-custom').css('height', '160px');
+                            console.log('INSIDE TIMEOUT');
                         }, 100);
                     }
-                    if (!scope.boolClientFilter && !scope.btnClicked) {
-                        scope.boolClientFilter = true;
-                    }  
-
+                    if (!scope.boolClientFilter && !scope.btnClicked) scope.boolClientFilter = true;
                 } 
                 else {
-
                     if (scope.boolChangeClass) {
                         $('.billing-block .fix-on-scroll').css('height', '465px');
                         $('.billing-block .boxes-wrap .box-custom').css('height', '235px');
                         setTimeout(function(){
                             scope.boolChangeClass = false;
-                        }, 200);
+                        }, 10);
                     }
-                    if (scope.boolClientFilter) {
-                        scope.boolClientFilter = false;
-                    }
-                    if (scope.btnClicked) {
-                        scope.btnClicked = false;
-                    }    
-
+                    if (scope.boolClientFilter) scope.boolClientFilter = false;
+                    if (scope.btnClicked) scope.btnClicked = false;
                 }
                 scope.$apply();
             });
